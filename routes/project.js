954ea3577c42bc9cdcfb8924ff69aa292738ddc7
Philip Guo <philip@pgbovine.net>
2016-10-02 15:37:05 -0700

    improved starter code a bit

--- routes/project.js
+++ routes/project.js
@@ -1,13 +1,13 @@
 var projects = require('../projects.json');
 
-exports.projectInfo = function(req, res) { 
-	var projectID = req.params.id;
-	if (projectID == "random") {
-		projectID = Math.floor(Math.random() * projects.length) + 1;
-	} else {
-		projectID = parseInt(projectID);
-	}
+exports.projectInfo = function(req, res) {
+  var projectID = req.params.id;
+  if (projectID == "random") {
+    projectID = Math.floor(Math.random() * projects.length) + 1;
+  } else {
+  projectID = parseInt(projectID);
+  }
 
-  	var project = projects[projectID-1]; // of by one, our first project has index 0
-  	res.json(project);
+  var project = projects[projectID-1]; // first project has index 0
+  res.json(project); // send JSON back to browser
 }
